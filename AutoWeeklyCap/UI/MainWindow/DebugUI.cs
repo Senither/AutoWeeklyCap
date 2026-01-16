a2f4700abb4735cd29b525414a1b093fec4608c8
Alexis Tan <alexis@sen-dev.com>
2026-01-16 04:06:27 +0100

    Add game data state to debug UI

--- AutoWeeklyCap/UI/MainWindow/DebugUI.cs
+++ AutoWeeklyCap/UI/MainWindow/DebugUI.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using AutoWeeklyCap.Helpers;
 using Dalamud.Bindings.ImGui;
+using Dalamud.Interface.Colors;
+using ECommons.GameHelpers;
+using FFXIVClientStructs.FFXIV.Client.Game.Control;
 
 namespace AutoWeeklyCap.UI.MainWindow;
 
@@ -25,6 +28,34 @@ public class DebugUI
         DebugButton("Extract", () => ExtractHelper.ExtractMateria(), false);
         DebugButton("Self Repair", () => RepairHelper.Repair());
         DebugButton("NPC Repair", () => RepairNPCHelper.Repair());
+
+        ImGui.Separator();
+        ImGui.Text("Game data state:");
+        ImGui.Text($"Position:  T:{Player.Territory.RowId} P:{Player.Position}");
+
+        var taget = "<no target>";
+        try
+        {
+            unsafe
+            {
+                var t = TargetSystem.Instance()->Target;
+
+                taget = $"{t->GetName()} [id: {t->BaseId}]";
+            }
+        }
+        catch (Exception)
+        {
+            // ignored
+        }
+
+        ImGui.Text($"Target:     {taget}");
+        ImGui.Text("State:       ");
+        StateText(() => PlayerHelper.IsReady, "Ready");
+        StateText(() => PlayerHelper.IsValid, "Valid");
+        StateText(() => PlayerHelper.IsOccupied, "Occupied");
+        StateText(() => PlayerHelper.IsJumping, "Jumping");
+        StateText(() => PlayerHelper.IsMoving, "Moving");
+        StateText(() => PlayerHelper.IsCasting, "Casting", seperator: false);
     }
 
     private static void DebugButton(string text, Action action, bool sameLine = true)
@@ -34,5 +65,26 @@ public class DebugUI
 
         if (ImGui.Button(text))
             action();
+    }
+
+    private static void StateText(Func<bool> value, string text, bool seperator = true)
+    {
+        ImGui.SameLine(0, 0);
+
+
+        try
+        {
+            ImGui.TextColored(value() ? ImGuiColors.HealerGreen : ImGuiColors.DPSRed, text);
+        }
+        catch (Exception)
+        {
+            ImGui.TextColored(ImGuiColors.DalamudOrange, text);
+        }
+
+        if (seperator)
+        {
+            ImGui.SameLine(0, 0);
+            ImGui.Text(" | ");
+        }
     }
 }
