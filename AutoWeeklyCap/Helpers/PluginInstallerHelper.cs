3491aec757cb31fe67b22d0a8676dca167b28980
Alexis Tan <alexis@sen-dev.com>
2026-04-19 01:03:19 +0200

    Change plugin install to only install plugin repos
    instead of installing plugins for the user, the installer now only ensures the plugin is available for installation by adding the repo if it's not already added, and then opening the plugin installer for the user so they can manually choose to install the plugin or not.
    That way we're not automatically installing plugins for people

--- AutoWeeklyCap/Helpers/PluginInstallerHelper.cs
+++ AutoWeeklyCap/Helpers/PluginInstallerHelper.cs
@@ -72,8 +72,6 @@ public static class PluginInstallerHelper
     }
 
     private const string MasterRepositoryUrl = "https://dalamud-plugins.senither.com/";
-    private const int MaxInstallRetries = 5;
-    private const int InstallThrottleMs = 250;
 
     public static bool InstallPlugin(string repositoryUrl, string internalName)
     {
@@ -87,63 +85,29 @@ public static class PluginInstallerHelper
             return false;
         }
 
-        AWC.Log.Debug($"PluginInstaller: Starting plugin installation for {internalName} ({repositoryUrl})");
+        var exposedPlugin = AWC.PluginInterface.InstalledPlugins.FirstOrDefault(plugin => plugin.InternalName == internalName);
+        if (exposedPlugin != null) {
+            if (IPCSubscriber.IsReady(internalName)) {
+                AWC.Log.Debug($"PluginInstaller: {internalName} is ready");
+                Notify.Success($"{internalName} is already installed");
+            } else {
+                AWC.Log.Debug($"PluginInstaller: {internalName} already installed but not ready");
+                Notify.Warning($"{internalName} is already installed, please enable the plugin in /xlplugins");
+                AWC.PluginInterface.OpenPluginInstallerTo(PluginInstallerOpenKind.AllPlugins, internalName);
+            }
 
-        var tries = 0;
-        Task<bool>? installTask = null;
-        IExposedPlugin? exposedPlugin = null;
+            return true;
+        }
 
-        AWC.TaskManager.Insert(() =>
-            {
-                if (!EzThrottler.Throttle($"InstallPlugin:InProgress:{internalName}", InstallThrottleMs)) {
-                    return false;
-                }
+        var installUrl = DalamudReflector.HasRepo(MasterRepositoryUrl) ? MasterRepositoryUrl : repositoryUrl;
 
-                if (tries > MaxInstallRetries) {
-                    AWC.Log.Debug($"PluginInstaller: reached matched attempts for {internalName}, stopping installation");
-                    Notify.Error($"Failed to install {internalName}");
+        if (!DalamudReflector.HasRepo(installUrl)) {
+            AWC.Log.Debug($"PluginInstaller: Installing new repository ({installUrl}) for plugin {internalName}");
+            DalamudReflector.AddRepo(installUrl, true);
+        }
 
-                    return true;
-                }
-
-                exposedPlugin ??= AWC.PluginInterface.InstalledPlugins.FirstOrDefault(plugin => plugin.InternalName == internalName);
-                if (exposedPlugin != null) {
-                    if (installTask is { Result: true }) {
-                        AWC.Log.Debug($"PluginInstaller: Successfully installed plugin {internalName}");
-                        Notify.Success($"{internalName} has been installed");
-                    } else if (IPCSubscriber.IsReady(internalName)) {
-                        AWC.Log.Debug($"PluginInstaller: {internalName} is ready");
-                        Notify.Success($"{internalName} is already installed");
-                    } else {
-                        AWC.Log.Debug($"PluginInstaller: {internalName} already installed but not ready");
-                        Notify.Warning($"{internalName} is already installed, please enable the plugin in /xlplugins");
-                        AWC.PluginInterface.OpenPluginInstallerTo(PluginInstallerOpenKind.AllPlugins, internalName);
-                    }
-
-                    return true;
-                }
-
-                if (installTask == null) {
-                    var installUrl = DalamudReflector.HasRepo(MasterRepositoryUrl) ? MasterRepositoryUrl : repositoryUrl;
-
-                    AWC.Log.Debug($"PluginInstaller: Installing plugin {internalName} from {installUrl}");
-                    installTask = DalamudReflector.AddPlugin(installUrl, internalName);
-
-                    return false;
-                }
-
-                if (!installTask.IsCompleted) {
-                    return false;
-                }
-
-                AWC.Log.Debug($"PluginInstaller: Failed to install plugin {internalName}");
-
-                tries++;
-                installTask = null;
-
-                return false;
-            }, $"install plugin: {internalName}"
-        );
+        AWC.PluginInterface.OpenPluginInstallerTo(PluginInstallerOpenKind.AllPlugins, internalName);
+        Notify.Info($"Please install {internalName} to get started with using it");
 
         return true;
     }
