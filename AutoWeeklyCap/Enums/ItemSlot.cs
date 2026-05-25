cdd232a318e3a0ef474581ef1e4605768de3afd5
Alexis Tan <alexis@sen-dev.com>
2026-05-25 17:56:40 +0200

    Create item type enum and helpers to use it

--- AutoWeeklyCap/Enums/ItemSlot.cs
+++ AutoWeeklyCap/Enums/ItemSlot.cs
@@ -20,25 +20,45 @@ public enum ItemSlot
 
 public static class ItemSlotExtensions
 {
-    public static uint GetSlot(this ItemSlot slot)
+    extension(ItemSlot slot)
     {
-        return slot switch
+        public uint GetSlot()
         {
-            ItemSlot.MainHand => 0,
-            ItemSlot.OffHand => 1,
+            return slot switch
+            {
+                ItemSlot.MainHand => 0,
+                ItemSlot.OffHand => 1,
 
-            ItemSlot.Head => 2,
-            ItemSlot.Body => 3,
-            ItemSlot.Hands => 4,
-            ItemSlot.Legs => 6,
-            ItemSlot.Feet => 7,
+                ItemSlot.Head => 2,
+                ItemSlot.Body => 3,
+                ItemSlot.Hands => 4,
+                ItemSlot.Legs => 6,
+                ItemSlot.Feet => 7,
 
-            ItemSlot.Earring => 8,
-            ItemSlot.Neckless => 9,
-            ItemSlot.Wrists => 10,
-            ItemSlot.RightRing => 11,
-            ItemSlot.LeftRing => 12,
-            _ => throw new ArgumentOutOfRangeException(nameof(slot), slot, null)
-        };
+                ItemSlot.Earring => 8,
+                ItemSlot.Neckless => 9,
+                ItemSlot.Wrists => 10,
+                ItemSlot.RightRing => 11,
+                ItemSlot.LeftRing => 12,
+
+                _ => throw new ArgumentOutOfRangeException(nameof(slot), slot, null)
+            };
+        }
+
+        public bool IsLeftSideItem()
+        {
+            return slot switch
+            {
+                ItemSlot.MainHand or ItemSlot.Head or ItemSlot.Body or ItemSlot.Hands or ItemSlot.Legs or ItemSlot.Feet => true,
+                ItemSlot.OffHand or ItemSlot.Earring or ItemSlot.Neckless or ItemSlot.Wrists or ItemSlot.RightRing or ItemSlot.LeftRing => false,
+
+                _ => throw new ArgumentOutOfRangeException(nameof(slot), slot, null)
+            };
+        }
+
+        public bool IsRightSideItem()
+        {
+            return !slot.IsLeftSideItem();
+        }
     }
 }
---
+++ AutoWeeklyCap/Enums/ItemType.cs
@@ -0,0 +1,62 @@
+﻿namespace AutoWeeklyCap.Enums;
+
+public enum ItemType
+{
+    Fending,
+    Healing,
+    Striking,
+    Maiming,
+    Slaying,
+    Scouting,
+    Aiming,
+    Casting,
+
+    Unknown
+}
+
+public static class ItemTypeExtensions
+{
+    public static ItemType GetItemTypeFromJobAndSlot(PlayerJob playerJob, ItemSlot slot)
+    {
+        return playerJob switch
+        {
+            PlayerJob.PLD or PlayerJob.WAR or PlayerJob.DRK or PlayerJob.GNB => ItemType.Fending,
+            PlayerJob.WHM or PlayerJob.SCH or PlayerJob.AST or PlayerJob.SGE => ItemType.Healing,
+            PlayerJob.BLM or PlayerJob.SMN or PlayerJob.RDM or PlayerJob.PCT => ItemType.Casting,
+            PlayerJob.BRD or PlayerJob.MCH or PlayerJob.DNC => ItemType.Aiming,
+            PlayerJob.MNK or PlayerJob.SAM => slot.IsLeftSideItem() ? ItemType.Striking : ItemType.Slaying,
+            PlayerJob.DRG or PlayerJob.RPR => slot.IsLeftSideItem() ? ItemType.Maiming : ItemType.Slaying,
+            PlayerJob.NIN or PlayerJob.VPR => slot.IsLeftSideItem() ? ItemType.Scouting : ItemType.Aiming,
+
+            _ => ItemType.Unknown
+        };
+    }
+
+    extension(ItemType itemType)
+    {
+        public bool IsWarGear()
+        {
+            return itemType switch
+            {
+                ItemType.Fending => true,
+                ItemType.Maiming => true,
+                ItemType.Striking => true,
+                ItemType.Scouting => true,
+                ItemType.Aiming => true,
+                ItemType.Slaying => true,
+
+
+                ItemType.Healing => false,
+                ItemType.Casting => false,
+                ItemType.Unknown => false,
+
+                _ => throw new ArgumentOutOfRangeException(nameof(itemType), itemType, null)
+            };
+        }
+
+        public bool IsMagicGear()
+        {
+            return !itemType.IsWarGear();
+        }
+    }
+}
