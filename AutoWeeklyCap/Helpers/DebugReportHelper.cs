4489a829efd941e8622eadf7eac29aac4d5b8eab
Alexis Tan <alexis@sen-dev.com>
2026-08-08 14:34:08 +0200

    Add plugin logs to debug and diagnostics output
    The code have also been moved into a separate helper class so it can be
    easily reused

---
+++ AutoWeeklyCap/Helpers/DebugReportHelper.cs
@@ -0,0 +1,113 @@
+﻿using AutoWeeklyCap.Config;
+
+using Dalamud.Plugin;
+
+using ECommons.Configuration;
+using ECommons.Logging;
+
+namespace AutoWeeklyCap.Helpers;
+
+public static class DebugReportHelper
+{
+    public static string? GenerateReport(bool prettyPrint = false)
+    {
+        return EzConfig.DefaultSerializationFactory.Serialize(
+            prettyPrint: prettyPrint,
+            config: new PluginDebugInformation(
+                PluginVersion: AWC.Version,
+                RunnerState: AWC.Runner.State,
+                Configuration: AWC.Config,
+                Player: GetPlayerDetails(),
+                HelperState: GetHelperState(),
+                InstalledPlugins: GetInstalledPlugins(),
+                LogMessages: InternalLog.Messages.ToArray()
+            )
+        );
+    }
+
+    private static PlayerDetails? GetPlayerDetails()
+    {
+        if (!Player.Available) {
+            return null;
+        }
+
+        return new PlayerDetails(
+            Player.CID,
+            Player.NameWithWorld,
+            Player.CurrentWorldName,
+            Player.Position,
+            Player.Territory.RowId,
+            Player.ClassJob.RowId,
+            Player.Level
+        );
+    }
+
+    private static Dictionary<string, object> GetHelperState()
+    {
+        Dictionary<string, object> state = new();
+        if (!Player.Available) {
+            return state;
+        }
+
+        state["CurrencyHelper#GetGil"] = CurrencyHelper.GetGil();
+        state["CurrencyHelper#GetUncappedAcquiredTomestoneCount"] = CurrencyHelper.GetUncappedAcquiredTomestoneCount();
+        state["CurrencyHelper#IsPlayerLimitedTomestoneCapped"] = CurrencyHelper.IsPlayerLimitedTomestoneCapped();
+        state["CurrencyHelper#IsPlayerWeeklyLimitedTomestoneCapped"] = CurrencyHelper.IsPlayerWeeklyLimitedTomestoneCapped();
+        state["CurrencyHelper#IsPlayerTotalLimitedTomestoneCapped"] = CurrencyHelper.IsPlayerTotalLimitedTomestoneCapped();
+        state["CurrencyHelper#GetLimitedTomestoneWeeklyLimit"] = CurrencyHelper.GetLimitedTomestoneWeeklyLimit();
+        state["CurrencyHelper#GetWeeklyAcquiredLimitedTomestoneCount"] = CurrencyHelper.GetWeeklyAcquiredLimitedTomestoneCount();
+        state["CurrencyHelper#GetTotalAcquiredLimitedTomestoneCount"] = CurrencyHelper.GetTotalAcquiredLimitedTomestoneCount();
+        state["GrandCompanyHelper#GetGrandCompany"] = GrandCompanyHelper.GetGrandCompany();
+        state["HousingHelper#IsInsideHouse"] = HousingHelper.IsInsideHouse();
+        state["HousingHelper#IsInsideApartment"] = HousingHelper.IsInsideApartment();
+        state["HousingHelper#IsInsideFC"] = HousingHelper.IsInsideFC();
+        state["LevelingHelper#GetCharacterToLevel"] = LevelingHelper.GetCharacterToLevel() ?? "<empty>";
+        state["PlayerHelper#IsReady"] = PlayerHelper.IsReady;
+        state["PlayerHelper#IsLoggedIn"] = PlayerHelper.IsLoggedIn;
+        state["PlayerHelper#IsOccupied"] = PlayerHelper.IsOccupied;
+        state["PlayerHelper#IsValid"] = PlayerHelper.IsValid;
+        state["PlayerHelper#CanSelfRepairWithCrafters"] = PlayerHelper.CanSelfRepairWithCrafters;
+        state["InventoryHelper#GetEmptySlotsInBag"] = InventoryHelper.GetEmptySlotsInBag();
+        state["InventoryHelper#IsAtleastOneArmoryChestSlotFull"] = InventoryHelper.IsAtleastOneArmoryChestSlotFull();
+        state["InventoryHelper#GetDeliverableItemsCount"] = InventoryHelper.GetDeliverableItemsCount();
+        state["InventoryHelper#GetLowestConditionEquippedItem"] = InventoryHelper.GetLowestConditionEquippedItem().Container;
+        state["InventoryHelper#GetDarkMatterCount"] = InventoryHelper.GetDarkMatterCount();
+        state["InventoryHelper#GetCurrentItemLevel"] = InventoryHelper.GetCurrentItemLevel();
+
+        return state;
+    }
+
+    private static Dictionary<string, bool> GetInstalledPlugins()
+    {
+        Dictionary<string, bool> plugins = new Dictionary<string, bool>();
+
+        foreach (IExposedPlugin plugin in AWC.PluginInterface.InstalledPlugins) {
+            plugins[plugin.Name] = plugin.IsLoaded;
+        }
+
+        return plugins;
+    }
+
+    // ReSharper disable NotAccessedPositionalProperty.Local
+    private record PluginDebugInformation(
+        string PluginVersion,
+        RunnerState RunnerState,
+        Configuration Configuration,
+        PlayerDetails? Player,
+        Dictionary<string, object> HelperState,
+        Dictionary<string, bool> InstalledPlugins,
+        InternalLogMessage[] LogMessages
+    );
+
+    private record PlayerDetails(
+        // ReSharper disable once InconsistentNaming
+        ulong CID,
+        string Name,
+        string CurrentWorld,
+        Vector3 Position,
+        uint Territory,
+        uint ClassJob,
+        int ClassLevel
+    );
+    // ReSharper restore NotAccessedPositionalProperty.Local
+}
--- AutoWeeklyCap/UI/Windows/ControlPanelWindow.cs
+++ AutoWeeklyCap/UI/Windows/ControlPanelWindow.cs
@@ -1,9 +1,7 @@
-﻿using AutoWeeklyCap.Config;
-using AutoWeeklyCap.UI.Helpers;
+﻿using AutoWeeklyCap.UI.Helpers;
 
 using Dalamud.Interface;
 using Dalamud.Interface.Windowing;
-using Dalamud.Plugin;
 
 using ECommons.Configuration;
 
@@ -17,15 +15,17 @@ public class ControlPanelWindow : Window
     {
         SizeConstraints = new WindowSizeConstraints { MinimumSize = new Vector2(575, 400), MaximumSize = new Vector2(9999, 9999) };
 
-        // @formatter:off
         TitleBarButtons.Add(new TitleBarButton
         {
-            Click = _ => CopyDebugReportToClipboard(),
+            Click = _ =>
+            {
+                ImGui.SetClipboardText(DebugReportHelper.GenerateReport());
+                Notify.Info("Debug & Diagnostics info has been copied to clipboard");
+            },
             Icon = FontAwesomeIcon.Code,
             IconOffset = new Vector2(2, 2),
             ShowTooltip = () => ImGui.SetTooltip("Copies debug and diagnostics plugin information to clipboard")
         });
-        // @formatter:on
     }
 
     public override void Draw()
@@ -57,85 +57,4 @@ public class ControlPanelWindow : Window
     {
         EzConfig.Save();
     }
-
-    private static void CopyDebugReportToClipboard()
-    {
-        var installedPlugins = new Dictionary<string, bool>();
-        foreach (IExposedPlugin plugin in AWC.PluginInterface.InstalledPlugins) {
-            installedPlugins[plugin.Name] = plugin.IsLoaded;
-        }
-
-        Dictionary<string, object> helperState = new();
-        PlayerDetails? player = null;
-
-        if (Player.Available) {
-            helperState["CurrencyHelper#GetGil"] = CurrencyHelper.GetGil();
-            helperState["CurrencyHelper#GetUncappedAcquiredTomestoneCount"] = CurrencyHelper.GetUncappedAcquiredTomestoneCount();
-            helperState["CurrencyHelper#IsPlayerLimitedTomestoneCapped"] = CurrencyHelper.IsPlayerLimitedTomestoneCapped();
-            helperState["CurrencyHelper#IsPlayerWeeklyLimitedTomestoneCapped"] = CurrencyHelper.IsPlayerWeeklyLimitedTomestoneCapped();
-            helperState["CurrencyHelper#IsPlayerTotalLimitedTomestoneCapped"] = CurrencyHelper.IsPlayerTotalLimitedTomestoneCapped();
-            helperState["CurrencyHelper#GetLimitedTomestoneWeeklyLimit"] = CurrencyHelper.GetLimitedTomestoneWeeklyLimit();
-            helperState["CurrencyHelper#GetWeeklyAcquiredLimitedTomestoneCount"] = CurrencyHelper.GetWeeklyAcquiredLimitedTomestoneCount();
-            helperState["CurrencyHelper#GetTotalAcquiredLimitedTomestoneCount"] = CurrencyHelper.GetTotalAcquiredLimitedTomestoneCount();
-            helperState["GrandCompanyHelper#GetGrandCompany"] = GrandCompanyHelper.GetGrandCompany();
-            helperState["HousingHelper#IsInsideHouse"] = HousingHelper.IsInsideHouse();
-            helperState["HousingHelper#IsInsideApartment"] = HousingHelper.IsInsideApartment();
-            helperState["HousingHelper#IsInsideFC"] = HousingHelper.IsInsideFC();
-            helperState["LevelingHelper#GetCharacterToLevel"] = LevelingHelper.GetCharacterToLevel() ?? "<empty>";
-            helperState["PlayerHelper#IsReady"] = PlayerHelper.IsReady;
-            helperState["PlayerHelper#IsLoggedIn"] = PlayerHelper.IsLoggedIn;
-            helperState["PlayerHelper#IsOccupied"] = PlayerHelper.IsOccupied;
-            helperState["PlayerHelper#IsValid"] = PlayerHelper.IsValid;
-            helperState["PlayerHelper#CanSelfRepairWithCrafters"] = PlayerHelper.CanSelfRepairWithCrafters;
-            helperState["InventoryHelper#GetEmptySlotsInBag"] = InventoryHelper.GetEmptySlotsInBag();
-            helperState["InventoryHelper#IsAtleastOneArmoryChestSlotFull"] = InventoryHelper.IsAtleastOneArmoryChestSlotFull();
-            helperState["InventoryHelper#GetDeliverableItemsCount"] = InventoryHelper.GetDeliverableItemsCount();
-            helperState["InventoryHelper#GetLowestConditionEquippedItem"] = InventoryHelper.GetLowestConditionEquippedItem().Container;
-            helperState["InventoryHelper#GetDarkMatterCount"] = InventoryHelper.GetDarkMatterCount();
-            helperState["InventoryHelper#GetCurrentItemLevel"] = InventoryHelper.GetCurrentItemLevel();
-
-            player = new PlayerDetails(
-                Player.CID,
-                Player.NameWithWorld,
-                Player.CurrentWorldName,
-                Player.Position,
-                Player.Territory.RowId,
-                Player.ClassJob.RowId,
-                Player.Level
-            );
-        }
-
-        ImGui.SetClipboardText(EzConfig.DefaultSerializationFactory.Serialize(new PluginDebugInformation(
-            AWC.Version,
-            AWC.Runner.State,
-            AWC.Config,
-            player,
-            helperState,
-            installedPlugins
-        )));
-
-        Notify.Info("Debug & Diagnostics info has been copied to clipboard");
-    }
-
-    // ReSharper disable NotAccessedPositionalProperty.Local
-    private record PluginDebugInformation(
-        string PluginVersion,
-        RunnerState RunnerState,
-        Configuration Configuration,
-        PlayerDetails? Player,
-        Dictionary<string, object> HelperState,
-        Dictionary<string, bool> InstalledPlugins
-    );
-
-    private record PlayerDetails(
-        // ReSharper disable once InconsistentNaming
-        ulong CID,
-        string Name,
-        string CurrentWorld,
-        Vector3 Position,
-        uint Territory,
-        uint ClassJob,
-        int ClassLevel
-    );
-    // ReSharper restore NotAccessedPositionalProperty.Local
 }
