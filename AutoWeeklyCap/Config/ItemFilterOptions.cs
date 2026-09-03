d776b7b739843aca832d65a82fcb2185dd8a5e95
Alexis Tan <alexis@sen-dev.com>
2026-09-03 13:17:44 +0200

    Change default values for item filter options

--- AutoWeeklyCap/Config/ItemFilterOptions.cs
+++ AutoWeeklyCap/Config/ItemFilterOptions.cs
@@ -3,10 +3,10 @@
 [Serializable]
 public class ItemFilterOptions
 {
-    public ItemPriceType ItemPriceType = ItemPriceType.Average;
+    public ItemPriceType ItemPriceType = ItemPriceType.Maximum;
 
     public uint GilThreshold { get; set; } = 1000;
-    public uint ItemLevelThreshold { get; set; } = 0;
+    public uint ItemLevelThreshold { get; set; } = Constants.CurrentMaxItemLevel - 145;
     public bool ExcludeMateria { get; set; } = false;
     public bool ExcludeFood { get; set; } = false;
     public bool ExcludePotions { get; set; } = false;
